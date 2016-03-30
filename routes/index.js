00a25e8c5b3cbbba661d95668d4dc1319cde3ad3
Austin Adams <adamsaustin104@gmail.com>
2016-03-29 22:07:36 -0600

    added login bar in header

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var crypto = require('crypto');
 
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Today I Learned' });
